3da6f2452bc97be8a30b96c44ce1ac66309476b1
Tanimu Saadu <tanimusaadu2@gmail.com>
2024-01-18 20:07:20 +1300

    modified

--- cfe/cfe/urls.py
+++ cfe/cfe/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
+from core import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls)
 ]
